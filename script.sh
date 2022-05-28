18eabe7ca38906c40feff2c80992a7283a3912ba
tduck973564 <tduck973564@gmail.com>
2022-05-28 17:33:05 +1000

    done but untested

--- script.sh
+++ script.sh
@@ -9,27 +9,27 @@ cd ~
 echo Adding Flathub to Flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-echo Installation of RPMFusion
-sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf groupupdate -y core
-sudo dnf groupupdate -y multimedia --setop="install_weak_deps=False"
+echo Installation of Packman
+sudo zypper ar -cfp 90 https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
+sudo zypper dup --from packman --allow-vendor-change
 
 echo Patch GNOME
 echo "[Settings]\ngtk-hint-font-metrics=1" >> ~/.config/gtk-4.0/settings.ini
-sudo dnf copr enable calcastor/gnome-patched
+sudo zypper ar https://download.opensuse.org/repositories/home:/tjyrinki_suse:/branches:/openSUSE:/Factory/openSUSE_Tumbleweed/home:tjyrinki_suse:branches:openSUSE:Factory.repo
 
-echo Update system before continuing
-sudo dnf --refresh upgrade -y
+echo Update System before continuing
+sudo zypper up
 
 echo Installation of Oh My Zsh!
-sudo dnf install util-linux-user zsh git
+sudo zypper in zsh git
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
 chsh -s /usr/bin/zsh
 sed -e s/robbyrussell/lukerandall/ ~/.zshrc > ~/.zshrc.tmp && mv ~/.zshrc.tmp ~/.zshrc
 
 echo Installation of GitHub CLI and setup of Git
-sudo dnf config-manager -y --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
-sudo dnf install -y gh
+sudo zypper addrepo https://cli.github.com/packages/rpm/gh-cli.repo
+sudo zypper ref
+sudo zypper in gh
 sh -c "gh auth login"
 
 echo "Type in your git username: "
@@ -42,27 +42,27 @@ git config --global user.email  $GITEMAIL
 
 echo Installation of VSCode
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo dnf install -y code
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/zypp/repos.d/vscode.repo'
+sudo zypper ref
+sudo zypper in code
 
 echo Installation of Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain stable --profile default -y
 
 echo Installation of Python
-sudo dnf install python pip
 pip install --upgrade pip
 pip install pylint
 curl -sSL https://install.python-poetry.org | python3 -
 
 echo Installation of TypeScript and JavaScript
-sudo dnf install nodejs
+sudo zypper in nodejs16
 sudo npm install -g typescript
 
 echo Installation of Java
-sudo dnf install -y java-17-openjdk java-17-openjdk-devel
+sudo zypper in java-17-openjdk java-17-openjdk-devel
 
 echo Installation of build-essential equivalent, clang, Meson and Ninja
-sudo dnf install -y make automake gcc gcc-c++ kernel-devel clang clang-tools-extra meson ninja-build
+sudo zypper in make automake gcc gcc-c++ kernel-devel clang clang-tools meson ninja
 
 echo Installation of JetBrains
 curl -fsSL https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install/master/jetbrains-toolbox.sh | bash
@@ -92,7 +92,7 @@ code --install-extension llvm-vs-code-extensions.vscode-clangd
 echo Set VSCode settings
 
 echo Installation of miscellaneous useful apps
-sudo dnf install -y discord ffmpeg pavucontrol pulseeffects gnome-tweaks
+sudo dnf install -y discord ffmpeg pavucontrol easyeffects gnome-tweaks
 flatpak install -y com.github.tchx84.Flatseal org.gnome.Extensions
 
 echo Log into accounts on web browser
@@ -106,54 +106,28 @@ mkdir ~/Repositories
 mkdir ~/Coding
 mkdir ~/Games
 
-echo Set up SSH and enable firewall to block all except on port 22
-sudo systemctl enable --now sshd
-sudo systemctl enable --now firewalld
-for i in $( ifconfig -a | sed 's/[ \t].*//;/^\(lo\|\)$/d' ); do
-    sudo firewall-cmd --zone=block --change-interface=$i
-    echo Added $i to block zone
-done
-sudo firewall-cmd --permanent --add-service=ssh
-
-echo Install nvidia drivers if nvidia gpu is installed
-if [[ $(lspci) = *NVIDIA* ]]; then
-  sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
-  echo "Wait 5 minutes before restarting to make sure the module is built"
-fi
-
 echo Install onedrive
-sudo dnf install -y onedrive
+sudo zypper in onedrive
 onedrive --synchronize
 systemctl --user enable onedrive
 systemctl --user start onedrive
 
 echo Download icon theme and fonts
-sudo dnf install -y papirus-icon-theme fira-code-fonts google-roboto-fonts ibm-plex-fonts-all rsms-inter-fonts materia-gtk-theme
+sudo zypper addrepo https://ftp.lysator.liu.se/pub/opensuse/repositories/M17N:/fonts/openSUSE_Tumbleweed/ fonts-x86_64
 
-<<comment 
-echo Fix font problems in SDDM
-sudo sh -c 'echo "<?xml version="1.0"?>
-<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-<fontconfig>
-<alias>
-    <family>sans-serif</family>
-    <prefer>
-    <family>Roboto</family>
-    <family>Noto Sans Symbols</family>
-    <family>Noto Sans Symbols2</family>
-    <family>Noto Emoji</family>
-    </prefer>
-</alias>
-</fontconfig>" > /etc/fonts/local.conf'
-comment
+sudo zypper in papirus-icon-theme fira-code-fonts ibm-plex-sans-fonts ibm-plex-mono-fonts ibm-plex-serif-fonts inter-fonts materia-gtk-theme
 
 echo Dotfiles
 git clone https://github.com/tduck973564/dotfiles ~/.dotfiles
 echo ". ~/.dotfiles/.aliases" >> ~/.zshrc
 
 echo "Fix inconsistent GNOME 42 theming; you will need to enable the theme in tweaks"
-sudo dnf copr enable nickavem/adw-gtk3 -y
-sudo dnf install adw-gtk3
+cd ~/Repositories
+git clone https://github.com/lassekongo83/adw-gtk3
+cd adw-gtk3
+meson build
+sudo ninja -C build install
+cd ~
 flatpak install -y org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark
 
 echo Install firefox theme
